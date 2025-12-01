27e9f9f0eba8a4ed03ef5dbb999c9c2912342f63
Zaxiriddin Xatamov <hatamovz047786@gmail.com>
2025-12-01 21:42:19 +0500

    detail qoshildi

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("detail-page/<int:article_id>/",views.article_detail, name="article_detail"),
+    path("filter-article/<int:category_id>/",views.filter_article, name="filter_article"),
 ]
--- app/views.py
+++ app/views.py
@@ -12,3 +12,35 @@ def home(request):
     }
 
     return render(request, "index.html", context)
+
+
+
+
+
+
+def article_detail(request, article_id):
+    article = models.Article.objects.get(id=article_id)
+    categorys = models.Category.objects.all()[:5]
+    top_article = models.Article.objects.filter(category=article.category)[:5]
+    data = list(top_article)
+    for i in data:
+        if i.id == article_id:
+            data.remove(i)
+
+    context = {
+        "article": article,
+        "categorys": categorys,
+        "top_article": data,
+    }
+
+    return render(request, "detail_page.html", context)
+
+
+def filter_article(request, category_id):
+    articles = models.Article.objects.filter(category_id=category_id)
+
+    context = {
+        "articles": articles,
+    }
+
+    return render(request, "filter_article.html", context)
